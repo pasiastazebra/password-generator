6896c8694b00ef5b12e5a5412bc7c7a4ea25750e
pasiastazebra <vobis08gw@gmail.com>
2023-07-08 22:38:49 +0200

    Update WindowContent.vue

--- password-generator/src/components/WindowContent.vue
+++ password-generator/src/components/WindowContent.vue
@@ -1,37 +1,52 @@
 <template>
-    <div class="window">
-        <h1 class="window-title">title placeholder</h1>
-        <div v-for="item in items" :key="item.id">
-            <WindowSection :text="item.text" :inputPlaceholder="item.inputPlaceholder"/>
-        </div>
+  <div class="window">
+    <h1 class="window-title">title placeholder</h1>
+    <div v-for="item in items" :key="item.id">
+      <WindowSection
+        :text="item.text"
+        :inputPlaceholder="item.inputPlaceholder"
+        :value="item.value"
+        v-if="item.id !== 5"
+      />
+      <WindowSection
+        v-else
+        :text="item.text"
+        :inputPlaceholder="item.inputPlaceholder"
+        :value="item.value"
+        @mouseover="showAlert"
+      />
     </div>
+  </div>
 </template>
-  
-  <script>
-  import WindowSection from './WindowComponents/WindowSection.vue';
 
-  export default {
-    name: 'WindowContent',
-    components: {
-        WindowSection,
-    },
-    data () {
-        return {
+<script>
+import WindowSection from './WindowComponents/WindowSection.vue';
+
+export default {
+  name: 'WindowContent',
+  components: {
+    WindowSection,
+  },
+  data() {
+    return {
       items: [
-        { id: 1, text: 'Length', inputPlaceholder: 'Length', value: '' },
-        { id: 2, text: 'Ammount of numbers', inputPlaceholder: 'Ammount of numbers', value: '' },
-        { id: 3, text: 'Ammount of special characters', inputPlaceholder: 'Ammount of special characters', value: '' },
-        { id: 4, text: 'Required phrase', inputPlaceholder: 'Required phrase', value: '' },
-        { id: 5, text: 'Generate', inputPlaceholder: 'Output', value: '' }
+        { id: 1, text: 'Length', inputPlaceholder: 'Length' },
+        { id: 2, text: 'Ammount of numbers', inputPlaceholder: 'Ammount of numbers' },
+        { id: 3, text: 'Ammount of special characters', inputPlaceholder: 'Ammount of special characters' },
+        { id: 4, text: 'Required phrase', inputPlaceholder: 'Required phrase' },
+        { id: 5, text: 'Generate', inputPlaceholder: 'Output' }
       ]
     };
-    },
-    
+  },
+  methods: {
+    showAlert() {
+      const length = this.items.find(item => item.id === 1).value;
+      alert(`${length}`);
+    }
   }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped lang="scss">
- 
-  </style>
-  
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
