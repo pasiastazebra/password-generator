f2c396c01ae4c0421d81b913f5a8613a71ca23a6
pasiastazebra <vobis08gw@gmail.com>
2023-07-11 13:13:14 +0200

    Update WindowContent.vue

--- password-generator/src/components/WindowContent.vue
+++ password-generator/src/components/WindowContent.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="window">
-    <h1 class="window-title">title placeholder</h1>
+    <h1 class="window-title">PASSWORD GENERATOR</h1>
     <div class="window-section">
       <h2 class="window-section-title">Length</h2>
-      <input type="number" class="window-section-input" placeholder="Length" :value="length">
+      <input type="number" class="window-section-input" placeholder="Length" v-model="length">
+      <p> {{ length }} </p>
     </div>
     <div class="window-section">
       <h2 class="window-section-title">Ammount of numbers</h2>
-      <input type="number" class="window-section-input" placeholder="Ammount of numbers" :value="ammountOfNumbers">
+      <input type="number" class="window-section-input" placeholder="Ammount of numbers" v-model="amountOfNumbers">
+      <p> {{ amountOfNumbers }} </p>
     </div>
     <div class="window-section">
       <h2 class="window-section-title">Ammount of special characters</h2>
-      <input type="number" class="window-section-input" placeholder="Ammount of special characters" :value="ammountOfSpecChar">
+      <input type="number" class="window-section-input" placeholder="Ammount of special characters" v-model="amountOfSpecChar">
+      <p> {{ amountOfSpecChar }} </p>
     </div>
     <div class="window-section">
       <h2 class="window-section-title">Required phrase</h2>
-      <input type="text" class="window-section-input" placeholder="Requeired phrase" :value="phrase">
+      <input type="text" class="window-section-input" placeholder="Requeired phrase" v-model="phrase">
+      <p> {{ phrase }} </p>
     </div>
     <div class="window-section">
       <h2 class="window-section-title">Result</h2>
-      <input type="number" class="window-section-input" placeholder="Result" :value="result">
+      <input type="text" class="window-section-input" placeholder="Result" :value="result" readonly>
+      <p> {{ result }} </p>
     </div>
   </div>
 </template>
@@ -28,6 +33,16 @@
 
 export default {
   name: 'WindowContent',
+
+  data() {
+    return {
+      length: '',
+      amountOfNumbers: '',
+      amountOfSpecChar: '',
+      phrase: '',
+      result: 'test'
+    };
+  }
 };
 </script>
 
