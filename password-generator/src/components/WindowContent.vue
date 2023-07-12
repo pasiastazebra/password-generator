d3804475b47f2ac7c9f0d5a40386a6215473bf9f
pasiastazebra <vobis08gw@gmail.com>
2023-07-13 00:21:05 +0200

    App is almost working
    There is a bug and result is generating by the wrong way.

--- password-generator/src/components/WindowContent.vue
+++ password-generator/src/components/WindowContent.vue
@@ -4,109 +4,93 @@
     <div class="window-section">
       <h2 class="window-section-title">Length</h2>
       <input type="number" class="window-section-input" placeholder="Length" v-model="length">
-      <p> {{ length }} </p>
+      <p>{{ length }}</p>
     </div>
     <div class="window-section">
       <h2 class="window-section-title">Ammount of numbers</h2>
       <input type="number" class="window-section-input" placeholder="Ammount of numbers" v-model="amountOfNumbers">
-      <p> {{ amountOfNumbers }} </p>
+      <p>{{ amountOfNumbers }}</p>
     </div>
     <div class="window-section">
       <h2 class="window-section-title">Ammount of special characters</h2>
       <input type="number" class="window-section-input" placeholder="Ammount of special characters" v-model="amountOfSpecChar">
     </div>
     <div class="window-section">
-      <h2 class="window-section-title" @mouseover="passwordGenerator">Result</h2>
+      <h2 class="window-section-title" @mouseenter="generatePassword">Result</h2>
       <input type="text" class="window-section-input" placeholder="Result" :value="result" readonly>
     </div>
   </div>
 </template>
 
 <script>
-//2D arrays for generating the password
-
 const letters = [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
-                 ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'], 
+                 ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'],
                  ['U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D']];
 
-const specChar = [['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'], 
-                  ['[', ']', '{', '}', '?', '<', '>', ',', '.', '+'], 
+const specChar = [['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'],
+                  ['[', ']', '{', '}', '?', '<', '>', ',', '.', '+'],
                   ['-', '=', '/', '|', '\\', '_', "'", '`', '"', '~']];
-
-let limitOfNumbers = 0;
-let limitOfCharacters = 0;
-
-//function for randomizing 
-
-const randomArray = () => {
-  return [Math.floor(Math.random() * 3), Math.floor(Math.random() * 9)];
-}
-
-//final password generating function have to be there
-const passwordGenerator = () => {
-  //limiters
-  let limitOfNumbers = parseInt(this.amountOfNumbers);
-  let limitOfCharacters = parseInt(this.amountOfSpecChar);
-  const allSymbols = parseInt(this.length);
-
-  if (allSymbols != limitOfCharacters + limitOfNumbers) {
-    this.result = 'too much symbols' ;
-  } else {
-      for (let i = 0; i <= allSymbols; i++) {
-        this.result[i] = this.charPicker();
-      }
-  }
-  console.log(this.result);
-}
-
-//it would be nice to separate it into more function for code clarity
-const charPicker = () => {
-  let [rowIndex, columnIndex] = randomArray();
-  if (limitOfNumbers === 0) {
-    if (limitOfCharacters === 0){
-      return letters[rowIndex][columnIndex];
-      //random to uper or lower case - to do - it can be separated function
-    }else {
-      limitOfCharacters--;
-      return specChar[rowIndex][columnIndex];
-    }
-  }
-  else if (limitOfCharacters === 0){
-    limitOfNumbers--;
-    return Math.floor(Math.random() * 9);
-  }else{
-    return letters[rowIndex][columnIndex];
-    //random to uper or lower case - to do - it can be separated function
-  }
-}
-
-
-console.log(charPicker(1, 0));
-console.log(charPicker(1, 1));
-console.log(charPicker(0, 0));
-console.log(charPicker(0, 1));
 
 export default {
   name: 'WindowContent',
-
   data() {
     return {
-      
       length: '',
       amountOfNumbers: '',
       amountOfSpecChar: '',
-      result: 'test',
-
+      result: '',
+      limitOfNumbers: 0,
+      limitOfCharacters: 0,
     };
   },
   methods: {
-    charPicker,
-    passwordGenerator,
-    randomArray,
-  }
+    randomArray() {
+      return [Math.floor(Math.random() * 3), Math.floor(Math.random() * 9)];
+    },
+
+    //it would be nice to separate it into more function for code clarity
+    charPicker() {
+      const [rowIndex, columnIndex] = this.randomArray();
+      if (this.limitOfNumbers === 0) {
+        if (this.limitOfCharacters === 0) {
+          return letters[rowIndex][columnIndex];
+          //random to uper or lower case - to do - it can be separated function
+        } else {
+          this.limitOfCharacters--;
+          return specChar[rowIndex][columnIndex];
+        }
+      } else if (this.limitOfCharacters === 0) {
+        this.limitOfNumbers--;
+        return Math.floor(Math.random() * 9);
+      } else {
+        return letters[rowIndex][columnIndex];
+        //random to uper or lower case - to do - it can be separated function
+      }
+    },
+
+    //final password generating function have to be there
+    passwordGenerator() {
+      //limiters
+      this.limitOfNumbers = parseInt(this.amountOfNumbers);
+      this.limitOfCharacters = parseInt(this.amountOfSpecChar);
+      const allSymbols = parseInt(this.length);
+
+      if (allSymbols !== this.limitOfCharacters + this.limitOfNumbers) {
+        this.result = 'too much symbols';
+      } else {
+        this.result = '';
+        for (let i = 0; i < allSymbols; i++) {
+          this.result += this.charPicker();
+        }
+      }
+    },
+    generatePassword() {
+      this.passwordGenerator();
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
-
+/* Twój styl CSS */
 </style>
